a6bbf926b084f6b1029e58c3afe245afb0a0b8c0
Khaled Nabil <kn2016a@gmail.com>
2020-01-18 20:41:42 +0200

    Updates

---
+++ precache-manifest.7211c8f3a64fac26f5e850ada63fa2b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bd8377154e0146f992c590b5a0ecbb0",
+    "url": "/monsters-rolodex2/index.html"
+  },
+  {
+    "revision": "8637426d24a97a271aca",
+    "url": "/monsters-rolodex2/static/css/main.8b965fd4.chunk.css"
+  },
+  {
+    "revision": "4a6ec64b958371189914",
+    "url": "/monsters-rolodex2/static/js/2.80ff7a43.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/monsters-rolodex2/static/js/2.80ff7a43.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8637426d24a97a271aca",
+    "url": "/monsters-rolodex2/static/js/main.624b0e70.chunk.js"
+  },
+  {
+    "revision": "c2289086910cec8a1a9f",
+    "url": "/monsters-rolodex2/static/js/runtime-main.8f9c5b86.js"
+  }
+]);
